f4f76db93ad8c567b01079b3b1ba3356afcd4c50
AL QOLBY ARIEF ENDALAPUTRA <putraendala@gmail.com>
2023-11-15 23:58:18 +0700

    bnerin lgi

--- app/Views/admin_obat.php
+++ app/Views/admin_obat.php
@@ -63,7 +63,7 @@
     
 </div>
 
-<a href="/admin/tambahobat"><button class="btn" style="background-color : #C67C4E; color:white; margin-left:40px; margin-top: 40px;">TAMBAH</button></a>
+<a href="<?=base_url('/admin/tambahobat')?>"><button class="btn" style="background-color : #C67C4E; color:white; margin-left:40px; margin-top: 40px;">TAMBAH</button></a>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
